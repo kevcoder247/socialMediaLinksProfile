6ca97f20bddc2c52695eaf25c24c12ab7820b7ef
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2024-07-19 11:22:37 -0400

    wip: add RWD styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./*html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors:{
+        neon: 'hsl(75, 94%, 57%)',
+      },
+      fontFamily:{
+        sans: [ "Inter", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
